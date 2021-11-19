e9ab1271bd0adb56f0399ee62211146e34c49bc2
Molly Wobbles <molly.rendon@outlook.com>
2021-11-18 19:23:12 -0500

    Added current date and added time block

---
+++ assets/script.js
@@ -0,0 +1,26 @@
+//Date
+var todaysDate = moment().format("DD MMM, YYYY hh:mm");
+console.log(todaysDate)
+
+$("p#currentDay").text(todaysDate);
+
+
+//Current Time
+var currentHour = moment().hour();
+console.log(currentHour)
+$('.description')
+  .each(function() {
+    var val = parseInt($(this).prop('id'));
+    if(val > currentHour) {
+      $(this).attr('class', 'future');
+    }else if(val < currentHour){
+      $(this).attr('class', 'past');
+    }else{
+      $(this).attr('class', 'present');
+    }
+  }
+  )
+
+
+
+
