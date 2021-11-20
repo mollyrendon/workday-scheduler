0692dd6e383ff2a8dc502fd991f4e96c8718752e
Molly Wobbles <molly.rendon@outlook.com>
2021-11-20 13:02:13 -0500

    Updated style sheet and added save data function

--- assets/script.js
+++ assets/script.js
@@ -21,6 +21,25 @@ $('.description')
   }
   )
 
-//Save Button
-
-
+  function saveData() {
+    var i = 9
+    var data = [];
+    while(i < 18) {
+      var el = document.getElementById(i).value;
+      data.push(el);
+      i++;
+    }
+    localStorage.setItem("storedData", JSON.stringify(data));
+      var testStorage = localStorage.getItem("storedData");
+   
+  }
+  
+  var j = 9;
+  var x = 0;
+  var toDo = JSON.parse(localStorage.getItem("storedData"));
+  console.log(toDo);
+  while(j < 18) {
+    document.getElementById(j).value = toDo[x];
+    x++
+    j++
+  }
